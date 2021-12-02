0e468037ef55417878c7b225acd18c1e2febe734
Nathan Lubawski <nathanlubawski1@gmail.com>
2021-12-02 17:53:21 -0300

    Melhorias em vetores

--- vetores.py
+++ vetores.py
@@ -1,12 +1,21 @@
-# 0 | 1 | 2 | 3 | 4
-# 5 | 2 | 4 | 6 | 1
-#
+"""
+Vetor é uma estrutura básica que é um espaço de memória geralmente contínuo e segmentado,
+onde cada posição de memória originada da segmentação pode ser utilizada para armazenar um elemento.
 
+Quando falamos em vetores, eles geralmente armazenam o mesmo tipo de dado em todas segmentações. Alem disso
+possuem um tamanho pré-determinado e acessamos as posicoes atraves de um indice numérico que começa em 0.
+
+Em alguns lugares vetor = array unidimensional, matriz = array bidimensional. Outra coisa, dependendo da linguagem
+podemos ter tanto a estrutura vetor quanto array, então vale sempre olhar a documentacao. O objetivo aqui e
+aprensentar algoritmos classicos, principalmente de busca e ordenacao. Para isso usaremos a estrutura de vetor
+construida a seguir.
+
+"""
 numeros = list()
-tamanho = int(input("Digite o tamanho do vetor: "))
+tamanho = int(input('Digite o tamanho do vetor: '))
 for i in range(tamanho):
-    # 0 .. 5
-    valor = int(input(f"Digite o número do vetor na posição {i}: "))
+    valor = int(input(f'Digite o número do vetor na posição {i}: '))
     numeros.append(valor)
-print("Vetor: ", numeros)
-print("Posição 1", numeros[1])
+
+#exibindo nosso vetor
+print('Vetor: ', numeros)
