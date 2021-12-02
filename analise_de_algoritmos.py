16feac66081bb3820b4e9cde45eef275e0688455
Nathan Lubawski <nathanlubawski1@gmail.com>
2021-12-02 16:51:49 -0300

    criacao do arquivo de analise de algoritmos e inicio do resumo teorico

---
+++ analise_de_algoritmos.py
@@ -0,0 +1,41 @@
+"""
+Analise de Algoritmos (ou de complexidade de algoritmos)
+
+Queremos saber como o tempo de execução e o consumo de memória é afetado por aumento da entrada.
+Ou seja, queremos o tempo/memoria em funcao do tamanho da entrada n (geralmente nos interessa
+n suficientemente grande).
+
+Vou descrever aqui as principais funcoes assintoticas, que devemos sempre ter em mente
+
+*---------------------------------------------------------------------------------------*
+-   Constante | Logaritmica | Linear | Sublinear | Quadratica | Cubica  | Exponencial   -
+-             |             |        |           |            |         |               -
+-      1      |    log(n)   |   n    | n log(n)  |     n²     |   n³    |      2^n      -
+*---------------------------------------------------------------------------------------*
+
+---------> ordem crescente de complexidade
+---------> log nesse contexto é sempre na base 2
+
+Exemplo de alguns algoritmos clássicos e sua complexidade
+Alguns deles serão implementados neste mesmo repositorio
+
+Constante: operacoes aritmeticas basicas, expressoes booleanas, desvio condicionais, atribuicao de variaveis,
+maximo em vetor ordenado, min em vetor ordenado, operacoes em pilha e fila
+
+Logaritmica: busca binaria(em lista ordenada), inserção/remoção em Heap, busca em profundidade em arvores binarias
+balanceadas
+
+Linear
+
+Sublinear
+
+Quadratica
+
+
+Cubica
+
+
+Ex
+
+
+"""
